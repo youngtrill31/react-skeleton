98cfbf94e49f51685e455d4e7ed4c3bc2f0d1509
Jeremiah De Leon <jeremiahdeleon@Jeremiahs-MacBook-Pro.local>
2016-04-23 15:40:55 -0700

    react-skeleton

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{"id": 1, "text":"ham"}
+                  ,{"id": 2, "text":"cheese"}];
+
+var List = React.createClass({
+  render: function() {
+    var listItems = ingredients.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+
+    return (<ul>{listItems}</ul>);
+  }
+});
+
+module.exports = List;
